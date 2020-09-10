8249f0c283002eae64463a8a7a9e0325b52b870f
gabriellmandoya <s218110444@student.unklab.ac.id>
2020-09-10 20:36:44 +0700

    Second Commit

---
+++ pertemuan_3/index.js
@@ -0,0 +1,29 @@
+/*
+    1. Variable Dan Tipe Data
+*/
+
+const firstName="John";
+let age=20;
+age=21;
+const isMarried=true;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(age+" "+isMarried);
+
+const info=
+    "My name is " + firstName + ", I'm " + age + " years old, " + isMarried;
+
+console.log(typeof info);
+
+
+
+/*
+    2. Operator
+*/
+
+let bilangan = 30;
+
+console.log(bilangan == "30"); //Loose Equality
+console.log(bilangan === "10"); //Strict Equality
